15bae15321167ba059d62d0d325a3613fc711062
Waffle-killr <Ethan_2001@yahoo.com>
2018-07-25 20:51:00 -0500

    Integrated edit, delete and create actions

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -18,4 +18,33 @@ class PortfoliosController < ApplicationController
       end
     end
   	end
+
+  	def edit
+  		@portfolio_item = Portfolio.find(params[:id])
+  	end
+
+    def update
+      @portfolio_item = Portfolio.find(params[:id])
+
+    respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'Your Portfolio Item was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+    end
+
+    def show
+      @portfolio_item = Portfolio.find(params[:id])
+    end
+
+    def destroy
+      @portfolio_item = Portfolio.find(params[:id])
+
+      Portfolio_item.destroy
+      respond_to do |format|
+        format.html { redirect_to portfolios_url, notice: 'Your Portfolio was removed'}
+      end
+    end
 end
